bce2c4021bc32a9148956fcf64da143cf54a3249
Oleksandr Balan <alex.balan.wewox@gmail.com>
2025-12-01 08:41:32 +0100

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,51 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    val data = Day01.parse(input)
+    println(Day01.part1(data))
+    println(Day01.part2(data))
+}
+
+private object Day01 {
+
+    data class Rotation(val value: Int)
+
+    fun parse(input: List<String>): List<Rotation> =
+        input.map {
+            val value = it.substring(1).toInt()
+
+            when {
+                it.startsWith("R") -> Rotation(+value)
+                it.startsWith("L") -> Rotation(-value)
+                else -> error("Unknown rotation for $it")
+            }
+        }
+
+    fun part1(rotations: List<Rotation>): Int {
+        var counter = 0
+        var value = 50
+        rotations.forEach {
+            value = (value + it.value) % 100
+            if (value == 0) {
+                counter += 1
+            }
+        }
+
+        return counter
+    }
+
+    fun part2(rotations: List<Rotation>): Int {
+        val oneByOneRotations = rotations.flatMap {
+            val value = abs(it.value)
+            if (it.value > 0) {
+                List(value) { Rotation(+1) }
+            } else {
+                List(value) { Rotation(-1) }
+            }
+        }
+
+        return part1(oneByOneRotations)
+    }
 }
